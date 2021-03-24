fe109f260c4a60fde3d5926c7a8d6e4795d293b6
HyoMook Yoon <jneekeyun@naver.com>
2021-03-24 10:29:56 +0900

    master branch

---
+++ InterfacePrograms/src/main/java/org/techtown/interfaceprograms/BankAccout.java
@@ -0,0 +1,17 @@
+package org.techtown.interfaceprograms;
+
+public class BankAccout {
+    private double balance;
+    private int accountNumber;
+    private static int lastAssingnedNumber=1000;
+
+    public BankAccout(double balance) {
+        this.balance = balance;
+        lastAssingnedNumber++;
+        accountNumber = LastAssignedNumber;
+    }
+}
+
+    public double getBalance() {
+        return
+    }
